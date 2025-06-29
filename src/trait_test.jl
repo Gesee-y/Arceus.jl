bf11ef8908974c3e392f7fdf99ad33cc70cd54bd
Talom Laël <gesee37@gmail.com>
2025-06-29 23:14:17 +0100

    Update trait_test.jl

--- src/trait_test.jl
+++ src/trait_test.jl
@@ -1,3 +1,37 @@
+"""
+    @hastrait var.trait
+
+Return whether the variable `var` has the given `trait`
+
+## Example
+
+```julia
+
+@traitpool "ABCDEF" begin
+    @trait electro
+    @trait flame
+    @trait laser 2
+    @subpool roles begin
+        @trait attacker
+        @trait support
+        
+    end
+    @subpool meta 16-32 begin
+        @trait earlygame
+        @trait midgame
+        @trait lategame
+    end
+    @abstract_subpool reserve1 33-48
+    @abstract_subpool reserve2 8
+end
+
+@make_traitpool "ABCDEF" Pokemon begin
+    @trait electro
+    @trait flame
+end
+
+@hastrait Pokemon.flame
+"""
 macro hastrait(expression)
     #println(parse_walk_chain(expression))
     Temp = parse_walk_chain(expression)
